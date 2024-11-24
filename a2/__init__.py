8d79fd7c8af7778fa953bd462295f75dabb16524
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-24 11:53:54 +0100

    Inject main function directly

--- a2/__init__.py
+++ a2/__init__.py
@@ -1,6 +1,26 @@
 import check50
 import check50.c
 
+MAIN_FUNCTION = """
+int main(void)
+{
+    bmp_image* bmp = open_image("uni.bmp");
+
+    if (bmp == NULL) {
+        printf("Failed to load image\n");
+
+        return 1;
+    }
+
+    printf("Image loaded: %dx%d\n", bmp->width, bmp->height);
+
+    save_image(bmp, "output.bmp");
+    close_image(bmp);
+
+    return 0;
+}
+"""
+
 
 @check50.check()
 def exists():
@@ -20,9 +40,11 @@ def has_no_main_function():
 
 @check50.check(has_no_main_function)
 def functions_correctly_implemented():
-    check50.include('main.c', 'Makefile')
 
-    check50.c.run('make watermark').exit(0)
+    with open('watermark.c', 'a') as f:
+        f.write(MAIN_FUNCTION)
+
+    check50.c.compile('watermark.c')
     (check50.c.valgrind('./watermark')
      .stdout('Image loaded: 3x3', regex=False)
      .exit(0))
