fe81ac7c228a98937cc8b034fec5b572e3eb3ad3
Kuschel_Swein <kuschelswein@gmail.com>
2024-12-04 16:30:41 +0100

    Import compiles check differently

--- a3/__init__.py
+++ a3/__init__.py
@@ -3,8 +3,9 @@ import check50.c
 
 a1 = check50.import_checks('../a1')
 from a1 import *
+from a1 import compiles
 
-@check50.check(a1.compiles)
+@check50.check(compiles)
 def handles_missing_word_list():
     (check50.run('./search-zone random-file-that-does-not-exist.txt')
      .stdout('ERROR: Could not open random-file-that-does-not-exist.txt!', regex=False)
