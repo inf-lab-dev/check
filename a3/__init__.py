c1716c1317f1eb6f8116dfa57c2372b972b36ccc
Kuschel_Swein <kuschelswein@gmail.com>
2024-12-04 16:31:39 +0100

    Fix loading wrong module

--- a3/__init__.py
+++ a3/__init__.py
@@ -1,11 +1,10 @@
 import check50
 import check50.c
 
-a1 = check50.import_checks('../a1')
-from a1 import *
-from a1 import compiles
+a2 = check50.import_checks('../a2')
+from a2 import *
 
-@check50.check(compiles)
+@check50.check(a2.compiles)
 def handles_missing_word_list():
     (check50.run('./search-zone random-file-that-does-not-exist.txt')
      .stdout('ERROR: Could not open random-file-that-does-not-exist.txt!', regex=False)
