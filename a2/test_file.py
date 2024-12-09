ac9d4f27c4c98d1c3f7eda407c9326e484edeac0
Kuschel_Swein <kuschelswein@gmail.com>
2024-12-09 17:46:48 +0100

    Add checks for `todo` task

---
+++ a2/test_file.py
@@ -0,0 +1,47 @@
+from todo import ToDo, todos, show
+import io
+
+
+def test_init_task():
+    assert ToDo('Learn Inf-Einf').task == 'Learn Inf-Einf'
+
+
+def test_init_completed():
+    assert not ToDo('Learn Inf-Einf').completed
+
+
+def test_update():
+    todo = ToDo('Learn Inf-Einf')
+    todo.update('Learn IntroSP')
+
+    assert todo.task == 'Learn IntroSP'
+    assert not todo.completed
+
+
+def test_complete():
+    todo = ToDo('Learn Inf-Einf')
+
+    assert not todo.completed
+
+    todo.complete()
+
+    assert todo.completed
+
+
+def test_output(capsys, monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('1\n0'))
+
+    todo1 = ToDo('Learn Inf-Einf')
+    todo2 = ToDo('Learn IntroSP')
+
+    todo2.complete()
+
+    todos.append(todo1)
+    todos.append(todo2)
+
+    show()
+
+    result = capsys.readouterr()
+
+    assert '[0] Learn Inf-Einf\n' in result.out
+    assert '[1] Learn IntroSP (completed)\n' in result.out
---
+++ a3/__init__.py
@@ -0,0 +1,113 @@
+import check50
+import check50.c
+
+a2 = check50.import_checks('../a2')
+from a2 import *
+
+@check50.check(exists)
+def create():
+    """The create function works correctly"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdout('ToDo created successfully!')
+        .stdin('1')
+        .stdout('[0] Learn Inf-Einf')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def complete():
+    """The complete function works correctly"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('3')
+        .stdin('0')
+        .stdout('Successfully completed the ToDo!')
+        .stdin('1')
+        .stdout('[0] Learn Inf-Einf (completed)')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def complete_invalid():
+    """The complete function rejects invalid indices"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('3')
+        .stdin('44')
+        .stdout('ERROR: .*')
+        .stdin('3')
+        .stdin('-4')
+        .stdout('ERROR: .*')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def delete():
+    """The delete function works correctly"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('4')
+        .stdin('0')
+        .stdout('Successfully deleted the ToDo!')
+        .stdin('1')
+        .stdout('No ToDos have been created yet.')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def delete_invalid():
+    """The delete function rejects invalid indices"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('4')
+        .stdin('44')
+        .stdout('ERROR: .*')
+        .stdin('4')
+        .stdin('-4')
+        .stdout('ERROR: .*')
+        .stdin('1')
+        .stdout('[0] Learn Inf-Einf')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def update():
+    """The update function works correctly"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('5')
+        .stdin('0')
+        .stdin('Learn IntroSP')
+        .stdout('Successfully updated the ToDo!')
+        .stdin('1')
+        .stdout('[0] Learn IntroSP')
+        .stdin('0')
+        .exit(0))
+
+
+@check50.check(create)
+def update_invalid():
+    """The update function rejects invalid indices"""
+    (check50.run('python3 todo.py')
+        .stdin('2')
+        .stdin('Learn Inf-Einf')
+        .stdin('5')
+        .stdin('44')
+        .stdout('ERROR: .*')
+        .stdin('5')
+        .stdin('-4')
+        .stdout('ERROR: .*')
+        .stdin('0')
+        .exit(0))
