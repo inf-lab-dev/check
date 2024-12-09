214ffa62ea874591201bec3da9ef203dbce26ba4
Kuschel_Swein <kuschelswein@gmail.com>
2024-12-09 17:56:17 +0100

    Load root test files manually

--- a3/test_file.py
+++ a3/test_file.py
@@ -1,1 +1,7 @@
-from ..a2.test_file import *
+import importlib
+
+spec = importlib.util.spec_from_file_location('root_test_file', '../a2/test_file.py')
+mod = importlib.util.module_from_spec(spec)
+root_test_file = spec.loader.exec_module(mod)
+
+from root_test_file import *
