8b12ff5c72d70d70e84adfa1be89cd731e0ec8f3
Kuschel_Swein <kuschelswein@gmail.com>
2025-01-06 16:46:23 +0100

    Simplify a4 tests

--- a4/test_a4.py
+++ a4/test_a4.py
@@ -3,13 +3,10 @@ from main import main
 
 
 @patch('main.fetch_weather')
-@patch('main.extract_data')
-def test_main(mock_extract_data, mock_fetch_weather):
+def test_main(mock_fetch_weather):
     mock_fetch_weather.return_value = 'Bamberg: ⛅️  🌡️+5°C 🌬️↑10km/h'
 
     with patch('main.CITIES_LIST', ['Bamberg']):
         main()
 
     mock_fetch_weather.assert_called_once_with('Bamberg')
-    mock_extract_data.assert_called_once_with(
-        'Bamberg: ⛅️  🌡️+5°C 🌬️↑10km/h')
